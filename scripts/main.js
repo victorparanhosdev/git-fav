ea986a159f4c7dca9530a9c72f9178524d7780a1
Victor Paranhos <victor_paranhos@hotmail.com>
2023-01-25 16:19:09 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,81 @@
-const tr = `<td>
-  <img
-    class="img-user"
-    src="https://avatars.githubusercontent.com/u/2254731?v=4"
-    alt="Foto da pessoa"
-  />
+class Favorites {
+  constructor(root) {
+    this.app = document.querySelector(root)
+    this.body = document.querySelector('main table #tbody')
+
+
+  }
+}
+
+class Gitfav extends Favorites {
+  constructor(root) {
+    super(root)
+    this.dadosRow()
+    this.updateRow()
+ 
+
+  
+
+  }
+
+  dadosRow(){
+    this.dados = [{
+      nome: 'Victor',
+      user: 'victorparanhosdev',
+      repositorios: 4252,
+      seguidores: 55577
+    },
+    {
+      nome: 'Jose',
+      user: 'Sosdev',
+      repositorios: 42342,
+      seguidores: 55337
+    },
+  ]
+
+
+
+  }
+
+  updateRow(){
+    const row = this.createRow()
+ 
+
+
+
+    this.dados.forEach(user => {
+     
+
+    })
+
+    this.body.append(row)
+
+  }
+
+  createRow() {
+
+    const tr = `
+  <td>
+  <img class="img-user" src="https://avatars.githubusercontent.com/u/2254731?v=4" alt="Foto da pessoa"/>
   <div>
-    <a href="">Nome da Pessoa</a>
-    <p>/usuariodapessoa</p>
+    <a class="nome" href="">Nome da Pessoa</a>
+    <p class="usuario">/usuariodapessoa</p>
   </div>
-</td>
-<td>242</td>
-<td>41414</td>
-<td>
+  </td>
+  <td class="repositorios">242</td>
+  <td class="seguidores">41414</td>
+  <td>
   <button type="button" class="btn-remover">Remover</button>
-</td>`
+  </td>`
+
+    return tr
+  }
+
+
+
+}
+
+
+
+
+new Gitfav('#app')
